6c1e79c03397be7c40a9750a854efe61b7a29662
mourya-s89 <kadali.mourya.s89@kalvium.community>
2025-06-07 11:50:43 +0530

    prac demo1

--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,1 +1,22 @@
 // write the component code here
+import React from 'react'
+
+const usercard = () => {
+  const profilePhoto = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSLU5_eUUGBfxfxRd4IquPiEwLbt4E_6RYMw&s'; 
+  const name = 'kadali mourya';
+  const email = 'kadali.mourya.s89@kalvium.community';
+  const phoneNumber = '+91 1234567890';
+  const address = '1234 Galaxy St, Milky Way, Earth';
+
+  return (
+    <div className="usercard">
+      <img src={profilePhoto} alt="Profile" className="profile-photo" />
+      <h2>{name}</h2>
+      <p>Email: {email}</p>
+      <p>Phone: {phoneNumber}</p>
+      <p>Address: {address}</p>
+    </div>
+  )
+}
+
+export default usercard
